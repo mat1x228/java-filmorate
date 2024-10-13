9937c8634683ae80d8c5dc58f554e29079f0e8a0
mat1x228 <boooo211@yandex.ru>
2024-10-13 15:47:30 +0300

    first commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/utils/ReleaseDateValidator.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.utils;
+
+import ru.yandex.practicum.filmorate.annotations.ReleaseDate;
+
+import java.time.LocalDate;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ReleaseDateValidator implements ConstraintValidator<ReleaseDate, LocalDate> {
+
+    @Override
+    public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext context) {
+        if (releaseDate == null) {
+            return true;
+        }
+        LocalDate minDate = LocalDate.of(1895, 12, 28);
+        return !releaseDate.isBefore(minDate);
+    }
+}
